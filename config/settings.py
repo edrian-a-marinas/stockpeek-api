5fddfdfc504bf01045cd012be9aa355e0592ced7
Edrian Mariñas <edrian.a.marinas@gmail.com>
2026-08-02 15:42:58 +0800

    feat: configure Daphne ASGI server

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -79,6 +80,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+ASGI_APPLICATION = "config.asgi.application"
 
 
 # Database
@@ -112,7 +114,15 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "simple": {
-            "format": "%(levelname)s:     %(name)s - %(message)s",
+            "()": "colorlog.ColoredFormatter",
+            "format": "%(log_color)s%(levelname)s:%(reset)s     %(name)s - %(message)s",
+            "log_colors": {
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
+            },
         },
     },
     "handlers": {
