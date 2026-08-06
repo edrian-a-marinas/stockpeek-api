f360752d3cd09c7c998cd4fb7edf950db8c056e1
Edrian Mariñas <edrian.a.marinas@gmail.com>
2026-08-06 11:49:26 +0800

    feat(admin): register account stock and watchlist models

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import StockInsight, StockPrice
+
+admin.site.register(StockPrice)
+admin.site.register(StockInsight)
--- watchlist/admin.py
+++ watchlist/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import StockNote, WatchlistItem
+
+admin.site.register(WatchlistItem)
+admin.site.register(StockNote)
